0ebfc2cf23b698c723b002c1bfad0a4baa8f9888
Zach Baskin <zbaskin@gatech.edu>
2023-01-31 15:13:21 -0500

    Problem 7: Reverse Integer

---
+++ problem7.js
@@ -0,0 +1,9 @@
+var reverse = function(x) {
+    var negative = x < 0;
+    var reverseArray = x.toString().split('').reverse();
+    if (negative) reverseArray.splice(0, -1);
+    var reverse = parseInt(reverseArray.join(''));
+    if (reverse > 0x7FFFFFFF) return 0;
+    if (negative) reverse = 0 - reverse;
+    return reverse;
+};
